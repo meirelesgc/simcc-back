c857f195852ee7483b095390e1d6760e31244f97
meirelesgc <geu_costa@outlook.com>
2025-06-01 14:43:42 -0300

    ...

--- init.sql
+++ init.sql
@@ -880,6 +880,323 @@ CREATE INDEX ON brand USING gin (title gin_trgm_ops);
 CREATE INDEX ON software USING gin (title gin_trgm_ops);
 CREATE INDEX ON event_organization USING gin (title gin_trgm_ops);
 
+--- admin
+-- Criação dos novos esquemas
+CREATE SCHEMA IF NOT EXISTS admin;
+CREATE SCHEMA IF NOT EXISTS admin_ufmg;
+
+-- Tabela Institution no esquema admin
+CREATE TABLE IF NOT EXISTS admin.institution(
+    institution_id uuid DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    acronym VARCHAR(50) UNIQUE,
+    lattes_id CHAR(16),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (institution_id)
+);
+
+-- Tabela Researcher no esquema admin
+CREATE TABLE IF NOT EXISTS admin.researcher(
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    name VARCHAR(150) NOT NULL,
+    lattes_id VARCHAR(20),
+    extra_field VARCHAR(255),
+    status BOOL NOT NULL DEFAULT True,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (researcher_id),
+    UNIQUE (lattes_id)
+);
+
+-- Tabela de Junção Researcher_Institution no esquema admin
+CREATE TABLE IF NOT EXISTS admin.researcher_institution(
+    researcher_institution_id uuid DEFAULT uuid_generate_v4(),
+    researcher_id uuid NOT NULL,
+    institution_id uuid NOT NULL,
+    start_date DATE DEFAULT CURRENT_DATE, -- Opcional: Data de início do vínculo
+    end_date DATE, -- Opcional: Data de fim do vínculo
+    is_current BOOLEAN DEFAULT TRUE, -- Opcional: Indica se é o vínculo atual
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (researcher_institution_id),
+    FOREIGN KEY (researcher_id) REFERENCES admin.researcher (researcher_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (institution_id) REFERENCES admin.institution (institution_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (researcher_id, institution_id, is_current) -- Garante que um pesquisador só tenha um vínculo atual com uma instituição
+);
+-- Tabela Graduate_Program no esquema admin
+CREATE TABLE IF NOT EXISTS admin.graduate_program(
+      graduate_program_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+      code VARCHAR(100) UNIQUE,
+      name VARCHAR(100) NOT NULL,
+      area VARCHAR(100) NOT NULL,
+      modality VARCHAR(100) NOT NULL,
+      TYPE VARCHAR(100) NULL,
+      rating VARCHAR(5),
+      institution_id uuid NOT NULL,
+      state character varying(4) DEFAULT 'BA'::character varying,
+      city character varying(100) DEFAULT 'Salvador'::character varying,
+      region character varying(100) DEFAULT 'Nordeste'::character varying,
+      url_image VARCHAR(200) NULL,
+      acronym character varying(100),
+      description TEXT,
+      visible bool DEFAULT FALSE,
+      site TEXT,
+      menagers TEXT[],
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (graduate_program_id),
+      FOREIGN KEY (institution_id) REFERENCES admin.institution (institution_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela Graduate_Program_Researcher no esquema admin
+CREATE TABLE IF NOT EXISTS admin.graduate_program_researcher(
+      graduate_program_id uuid NOT NULL,
+      researcher_id uuid NOT NULL,
+      year INT [],
+      type_ relationship,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (graduate_program_id, researcher_id),
+      FOREIGN KEY (researcher_id) REFERENCES admin.researcher (researcher_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      FOREIGN KEY (graduate_program_id) REFERENCES admin.graduate_program (graduate_program_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela Graduate_Program_Student no esquema admin
+CREATE TABLE IF NOT EXISTS admin.graduate_program_student(
+      graduate_program_id uuid NOT NULL,
+      researcher_id uuid NOT NULL,
+      year INT [],
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (graduate_program_id, researcher_id, year),
+      FOREIGN KEY (researcher_id) REFERENCES admin.researcher (researcher_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      FOREIGN KEY (graduate_program_id) REFERENCES admin.graduate_program (graduate_program_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela Weights no esquema admin
+CREATE TABLE IF NOT EXISTS admin.weights (
+      institution_id uuid PRIMARY KEY,
+      a1 numeric(20, 3),
+      a2 numeric(20, 3),
+      a3 numeric(20, 3),
+      a4 numeric(20, 3),
+      b1 numeric(20, 3),
+      b2 numeric(20, 3),
+      b3 numeric(20, 3),
+      b4 numeric(20, 3),
+      c numeric(20, 3),
+      sq numeric(20, 3),
+      book numeric(20, 3),
+      book_chapter numeric(20, 3),
+      software character varying,
+      patent_granted character varying,
+      patent_not_granted character varying,
+      report character varying,
+      f1 numeric(20, 3) DEFAULT 0,
+      f2 numeric(20, 3) DEFAULT 0,
+      f3 numeric(20, 3) DEFAULT 0,
+      f4 numeric(20, 3) DEFAULT 0,
+      f5 numeric(20, 3) DEFAULT 0,
+      FOREIGN KEY (institution_id) REFERENCES admin.institution (institution_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela Roles no esquema admin
+CREATE TABLE IF NOT EXISTS admin.roles (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      role VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Tabela Permission no esquema admin
+CREATE TABLE IF NOT EXISTS admin.permission (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      role_id UUID NOT NULL REFERENCES admin.roles(id) ON DELETE CASCADE ON UPDATE CASCADE,
+      permission VARCHAR(255) NOT NULL,
+      UNIQUE (role_id, permission)
+);
+
+-- Tabela Users no esquema admin
+CREATE TABLE IF NOT EXISTS admin.users (
+    user_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    display_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    uid VARCHAR(255) UNIQUE NOT NULL,
+    photo_url TEXT,
+    lattes_id VARCHAR(255),
+    institution_id uuid,
+    provider VARCHAR(255),
+    linkedin VARCHAR(255),
+    verify bool DEFAULT FALSE,
+    shib_id VARCHAR(255),
+    shib_code VARCHAR(255),
+    birth_date VARCHAR(10),
+    course_level VARCHAR(255),
+    first_name VARCHAR(255),
+    registration VARCHAR(255),
+    gender VARCHAR(50),
+    last_name VARCHAR(255),
+    email_status VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (institution_id) REFERENCES admin.institution (institution_id) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Tabela Users_Roles no esquema admin
+CREATE TABLE IF NOT EXISTS admin.users_roles (
+      role_id UUID NOT NULL,
+      user_id UUID NOT NULL,
+      PRIMARY KEY (role_id, user_id),
+      FOREIGN KEY (user_id) REFERENCES admin.users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      FOREIGN KEY (role_id) REFERENCES admin.roles (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela Newsletter_Subscribers no esquema admin
+CREATE TABLE IF NOT EXISTS admin.newsletter_subscribers (
+      id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      subscribed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabela Feedback no esquema admin
+CREATE TABLE IF NOT EXISTS admin.feedback (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    rating INTEGER CHECK (rating >= 0 AND rating <= 10) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--- SCHEMA admin_ufmg ---
+
+-- Tabela admin_ufmg.Researcher
+CREATE TABLE IF NOT EXISTS admin_ufmg.researcher (
+    researcher_id UUID PRIMARY KEY REFERENCES admin.researcher(researcher_id) ON DELETE CASCADE ON UPDATE CASCADE,
+
+    -- Campos comuns
+    full_name VARCHAR(255),
+    gender VARCHAR(255),
+    status_code VARCHAR(255),
+    work_regime VARCHAR(255),
+    job_class CHAR(25),
+    job_title VARCHAR(255),
+    job_rank VARCHAR(255),
+    job_reference_code VARCHAR(255),
+    academic_degree VARCHAR(255),
+    organization_entry_date DATE,
+    last_promotion_date DATE,
+
+    -- Novos campos
+    employment_status_description VARCHAR(255),
+    department_name VARCHAR(255),
+    career_category VARCHAR(255),
+    academic_unit VARCHAR(255),
+    unit_code VARCHAR(255),
+    function_code VARCHAR(255),
+    position_code VARCHAR(255),
+    leadership_start_date DATE,
+    leadership_end_date DATE,
+    current_function_name VARCHAR(255),
+    function_location VARCHAR(255),
+
+    -- Campos que estavam só na tabela antiga
+    registration_number VARCHAR(200),
+    ufmg_registration_number VARCHAR(200),
+    semester_reference VARCHAR(6)
+);
+
+-- Tabela admin_ufmg.Technician
+CREATE TABLE IF NOT EXISTS admin_ufmg.technician (
+    technician_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    -- Campos comuns
+    full_name VARCHAR(255),
+    gender VARCHAR(255),
+    status_code VARCHAR(255),
+    work_regime VARCHAR(255),
+    job_class CHAR(25),
+    job_title VARCHAR(255),
+    job_rank VARCHAR(255),
+    job_reference_code VARCHAR(255),
+    academic_degree VARCHAR(255),
+    organization_entry_date DATE,
+    last_promotion_date DATE,
+
+    -- Novos campos
+    employment_status_description VARCHAR(255),
+    department_name VARCHAR(255),
+    career_category VARCHAR(255),
+    academic_unit VARCHAR(255),
+    unit_code VARCHAR(255),
+    function_code VARCHAR(255),
+    position_code VARCHAR(255),
+    leadership_start_date DATE,
+    leadership_end_date DATE,
+    current_function_name VARCHAR(255),
+    function_location VARCHAR(255),
+
+    -- Campos antigos
+    registration_number VARCHAR(255),
+    ufmg_registration_number VARCHAR(255),
+    semester_reference VARCHAR(6)
+);
+
+-- Tabela admin_ufmg.Department
+CREATE TABLE IF NOT EXISTS admin_ufmg.department (
+      dep_id VARCHAR(20) PRIMARY KEY,
+      org_cod VARCHAR(3),
+      dep_nom VARCHAR(100),
+      dep_des TEXT,
+      dep_email VARCHAR(100),
+      dep_site VARCHAR(100),
+      dep_sigla VARCHAR(30),
+      dep_tel VARCHAR(20),
+      img_data BYTEA
+);
+
+-- Tabela admin_ufmg.Department_Technician
+CREATE TABLE IF NOT EXISTS admin_ufmg.department_technician (
+      dep_id VARCHAR(20),
+      technician_id uuid,
+      PRIMARY KEY (dep_id, technician_id),
+      FOREIGN KEY (dep_id) REFERENCES admin_ufmg.department (dep_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      FOREIGN KEY (technician_id) REFERENCES admin_ufmg.technician (technician_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela admin_ufmg.Department_Researcher
+CREATE TABLE IF NOT EXISTS admin_ufmg.department_researcher (
+      dep_id VARCHAR(20),
+      researcher_id uuid NOT NULL,
+      PRIMARY KEY (dep_id, researcher_id),
+      FOREIGN KEY (dep_id) REFERENCES admin_ufmg.department (dep_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      FOREIGN KEY (researcher_id) REFERENCES admin.researcher (researcher_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela admin_ufmg.Disciplines
+CREATE TABLE IF NOT EXISTS admin_ufmg.disciplines (
+      discipline_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      dep_id VARCHAR(20),
+      semester VARCHAR(20),
+      department VARCHAR(255),
+      academic_activity_code VARCHAR(255),
+      academic_activity_name VARCHAR(255),
+      academic_activity_ch VARCHAR(255),
+      demanding_courses VARCHAR(255),
+      oft VARCHAR(50),
+      available_slots VARCHAR(50),
+      occupied_slots VARCHAR(50),
+      percent_occupied_slots VARCHAR(50),
+      schedule VARCHAR(255),
+      language VARCHAR(50),
+      researcher_id uuid [],
+      researcher_name VARCHAR [],
+      status VARCHAR(50),
+      workload VARCHAR [],
+      FOREIGN KEY (dep_id) REFERENCES admin_ufmg.department (dep_id) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
 COMMIT;
 
 ROLLBACK;
+
