105a40bad6c269a7adc934978623fadea3adefcf
gmcosta <geu_costa@outlook.com>
2025-08-08 09:29:19 -0300

    wip

--- scripts/insert_researcher_progrmas_csv.py
+++ scripts/insert_researcher_progrmas_csv.py
@@ -1,33 +1,40 @@
 import pandas as pd
-import psycopg
 import unidecode
 
 from simcc.repositories import conn_admin
 
 researchers = pd.read_csv('storage/csv/simcc_program_researchers.csv')
 
-
 for _, researcher in researchers.iterrows():
-    researcher['name'] = unidecode.unidecode(researcher['name'])
-    researcher['name'] = researcher['name'].replace(' ', '').lower()
+    normalized_name = (
+        unidecode.unidecode(researcher['name']).replace(' ', '').lower()
+    )
+    researcher['name'] = normalized_name
 
-    SCRIPT_SQL = """
-        SELECT researcher_id AS id FROM researcher
+    sql_find_researcher = """
+        SELECT researcher_id AS id 
+        FROM researcher
         WHERE REPLACE(UNACCENT(name), ' ', '') ILIKE %(name)s
-        """
+    """
     researcher_data = researcher.to_dict()
-    if db_data := conn_admin.select(SCRIPT_SQL, researcher_data, one=True):
-        researcher_data = {**db_data, **researcher_data}
-        try:
-            SCRIPT_SQL = """
-                INSERT INTO graduate_program_researcher
-                    (graduate_program_id, researcher_id, year, type_)
-                SELECT graduate_program_id, %(id)s, ARRAY[2024,2025,2023,2022,2021], %(type_)s
-                FROM graduate_program
-                WHERE code = %(code)s
-                """
-            conn_admin.exec(SCRIPT_SQL, researcher_data)
-        except psycopg.errors.UniqueViolation:
-            print('Unique violation: ', researcher_data['id'])
-        except Exception as e:
-            print(e)
+    db_data = conn_admin.select(sql_find_researcher, researcher_data, one=True)
+
+    if not db_data:
+        print(f'Pesquisador não encontrado: {researcher["name"]}')
+        continue
+
+    researcher_data.update(db_data)
+
+    sql_upsert = """
+        INSERT INTO graduate_program_researcher
+            (graduate_program_id, researcher_id, year, type_)
+        SELECT gp.graduate_program_id, %(id)s, %(year)s, %(type_)s
+        FROM graduate_program gp
+        WHERE gp.code = %(code)s
+        ON CONFLICT (graduate_program_id, researcher_id, year) DO UPDATE
+        SET type_ = COALESCE(graduate_program_researcher.type_, EXCLUDED.type_);
+    """
+    conn_admin.exec(sql_upsert, researcher_data)
+    print(
+        f'Registro processado: {researcher_data["id"]} - Ano {researcher_data["year"]}'
+    )
