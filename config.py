5d099bdf34582ba63db29909c67fcdd1db5f654e
gmcosta <geu_costa@outlook.com>
2025-09-18 00:43:02 -0300

    chore: configuração do compose

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-from pydantic import validator
 from pydantic_settings import BaseSettings
 
 
@@ -21,18 +20,6 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-
-    @validator("OPENAI_API_KEY", pre=True, always=True)
-    def check_openai_key(cls, v):
-        if v is None:
-            raise ValueError("OPENAI_API_KEY is required for production environments")
-        return v
-
-    @validator("ALTERNATIVE_CNPQ_SERVICE", pre=True, always=True)
-    def check_proxy(cls, v):
-        if v:
-            print("O Download dos XMLs serão feitos via proxy no Tupi")
-        return v
 
 
 settings = Settings()
